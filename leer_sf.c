8371ed0d57cf2b8962ad2e9acb706cce66127498
jcasben <jcasben02@gmail.com>
2024-02-22 19:54:06 +0100

    [UPDATE] Corrected the functionality

--- leer_sf.c
+++ leer_sf.c
@@ -43,15 +43,17 @@ int main(int argc, char **argv)
         printf("\n");
         i++;
     }*/
-    printf("RESERVAMOS UN BLOQUE Y LUEGO LO LIBERAMOS\n");
+    printf("\nRESERVAMOS UN BLOQUE Y LUEGO LO LIBERAMOS\n");
     int nblock = reservar_bloque();
+    bread(posSB, &SB);
     printf("Se ha reservado el bloque físico nº %d, que era el 1º libre indicado por el MB\n", nblock);
     printf("SB.cantBloquesLibres = %d\n", SB.cantBloquesLibres);
     liberar_bloque(nblock);
+    bread(posSB, &SB);
     printf("Liberamos ese bloque y despues SB.cantBloquesLibres = %d\n", SB.cantBloquesLibres);
-    printf("MAPA DE BITS CON BLOQUES DE METADATOS OCUPADOS\n");
+    printf("\nMAPA DE BITS CON BLOQUES DE METADATOS OCUPADOS\n");
     int value = leer_bit(0);
-    printf("posSb: 0 -> leer_bit(0) = %d\n", value);
+    printf("posSB: 0 -> leer_bit(0) = %d\n", value);
     value = leer_bit(1);
     printf("SB.PrimerBloqueMB: 1 -> leer_bit(1) = %d\n", value);
     value = leer_bit(13);
@@ -64,7 +66,7 @@ int main(int argc, char **argv)
     printf("SB.PrimerBloqueDatos: 3139 -> leer_bit(3139) = %d\n", value);
     value = leer_bit(99999);
     printf("SB.UltimoBloqueDatos: 99999 -> leer_bit(99999) = %d\n", value);
-    printf("DATOS DEL DIRECTORIO RAIZ\n");
+    printf("\nDATOS DEL DIRECTORIO RAIZ\n");
 
     struct tm *ts;
     char atime[80];
@@ -80,11 +82,11 @@ int main(int argc, char **argv)
     strftime(mtime, sizeof(mtime), "%a %Y-%m-%d %H:%M:%S", ts);
     ts = localtime(&inode.ctime);
     strftime(ctime, sizeof(ctime), "%a %Y-%m-%d %H:%M:%S", ts);
-    printf("tipo: %d\n", inode.tipo);
+    printf("tipo: %c\n", inode.tipo);
     printf("permisos: %d\n", inode.permisos);
-    printf("atime: %ld\n", inode.atime);
-    printf("ctime: %ld\n", inode.ctime);
-    printf("mtime: %ld\n", inode.mtime);
+    printf("atime: %s\n", atime);
+    printf("ctime: %s\n", ctime);
+    printf("mtime: %s\n", mtime);
     printf("nlinks: %d\n", inode.nlinks);
     printf("tamEnBytesLog: %d\n", inode.tamEnBytesLog);
     printf("numBloquesOcupados: %d\n", inode.numBloquesOcupados);
