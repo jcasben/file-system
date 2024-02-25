70333a000985dcc8e7f38d61dd4a93bf3372b5ff
linkcla <linkcla10@gmail.com>
2024-02-25 20:21:55 +0100

    [NEW] Added new functions and variables

--- ficheros_basico.h
+++ ficheros_basico.h
@@ -4,6 +4,11 @@
 
 #define posSB 0 // el superbloque se escribe en el primer bloque de nuestro FS
 #define tamSB 1
+#define NPUNTEROS (BLOCKSIZE / sizeof(unsigned int))   // 256 punteros por bloque
+#define DIRECTOS 12
+#define INDIRECTOS0 (NPUNTEROS + DIRECTOS)    // 268
+#define INDIRECTOS1 (NPUNTEROS * NPUNTEROS + INDIRECTOS0)    // 65.804
+#define INDIRECTOS2 (NPUNTEROS * NPUNTEROS * NPUNTEROS + INDIRECTOS1) // 16.843.020
 
 struct superbloque
 {
@@ -70,3 +75,6 @@ int liberar_bloque(unsigned int nbloque);
 int escribir_inodo(unsigned int ninodo, struct inodo *inodo);
 int leer_inodo(unsigned int ninodo, struct inodo *inodo);
 int reservar_inodo(unsigned char tipo, unsigned char permisos);
+int obtener_nRangoBL(struct inodo *inodo, unsigned int nblogico, unsigned int *ptr);
+int obtener_indice(unsigned int nblogico, int nivel_punteros);
+int traducir_bloque_inodo(struct inodo *inodo, unsigned int nblogico, unsigned char reservar);
