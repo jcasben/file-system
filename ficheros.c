5497d188cad18595d45a20e081841cc902be8f89
jcasben <jcasben02@gmail.com>
2024-02-29 15:43:46 +0100

    [UPDATE] Continued level 5

--- ficheros.c
+++ ficheros.c
@@ -11,6 +11,8 @@
 /// @return the number of bytes written. FALLO otherwise.
 int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offset, unsigned int nbytes)
 {
+    int written_bytes = 0;
+
     // Get the specified inode.
     struct inodo inode;
     leer_inodo(ninodo, &inode);
@@ -36,6 +38,8 @@ int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offse
         // the data of the block that we read. Then, write the updated block.       
         memcpy(buf_bloque + desp1, buf_original, nbytes);
         if (bwrite(nbfisico, buf_bloque) == FALLO) return FALLO;
+
+        return nbytes;
     }
     // Case where the writing affects to more than 1 block.
     else
@@ -52,15 +56,23 @@ int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offse
         // FASE 2: Intermediate logical blocks.
         for (size_t i = primerBL + 1; i < ultimoBL; i++)
         {
-            if (bwrite(nbfisico, buf_original + (BLOCKSIZE - desp1) + (i - primerBL - 1)) == FALLO) return FALLO;
+            if (written_bytes += bwrite(nbfisico, buf_original + (BLOCKSIZE - desp1) + (i - primerBL - 1)) == FALLO) return FALLO;
         }
         
         // FASE 3: Last logical block.
         nbfisico = traducir_bloque_inodo(&inode, ultimoBL, 1);
         if (bread(nbfisico, buf_bloque) == FALLO) return FALLO;
         memcpy(buf_bloque, buf_original + (nbytes - (desp2 + 1)), desp2 + 1);
-        if (bread(buf_bloque, nbfisico) == FALLO) return FALLO;
-        // TODO: actualizar la metainfo del inodo
+        if (bwrite(buf_bloque, nbfisico) == FALLO) return FALLO;
+        // If we have written further than the end of the file
+        // update tamEnBytesLog.
+        if (ultimoBL > inode.tamEnBytesLog)
+        {
+            inode.tamEnBytesLog = ultimoBL;
+            inode.ctime = time(NULL);
+        }
+        inode.mtime = time(NULL);
+        if (escribir_inodo(ninodo, &inode) == FALLO) return FALLO;
         // TODO: devolver la cantidad de bytes escritos
     }
 } 
@@ -85,45 +97,93 @@ int mi_read_f(unsigned int ninodo, void *buf_original, unsigned int offset, unsi
         return FALLO;
     }
 
+    // If offset is greather than inode.tamEnBytesLog we cant read (read_bytes will be 0)
+    if (offset >= inode.tamEnBytesLog) return read_bytes;
+
+    // If we want to read more bytes than the file has, we read from offset to the end of the file.
+    // So we modify the nbytes to read.
+    if ((offset + nbytes) >= inode.tamEnBytesLog)
+    {
+        nbytes = inode.tamEnBytesLog - offset;
+    }
+
     int primerBL = offset / BLOCKSIZE;
     int ultimoBL = (offset + nbytes - 1) / BLOCKSIZE;
     int desp1 = offset % BLOCKSIZE;
     int desp2 = (offset + nbytes - 1) % BLOCKSIZE;
 
-    // if offset is greather than inode.tamEnBytesLog we cant read (read_bytes will be 0)
-    if (offset >= inode.tamEnBytesLog) return read_bytes;
-    if ((offset + nbytes) >= inode.tamEnBytesLog)
-    {
-        nbytes = inode.tamEnBytesLog - offset;
-        //leer solo 
-    }
-    //
     if(primerBL == ultimoBL)
     {
         int nbfisico = traducir_bloque_inodo(&inode, primerBL, 0);
         if (nbfisico != -1)
         {
             char buf_bloque[BLOCKSIZE];
+            // Read the block where the data is stored.
             if(bread(nbfisico, buf_bloque) == FALLO) return FALLO;
-            memcpy(buf_bloque + desp1, buf_original, nbytes); // es nbytes?
-            // not finished
+            // Copy the slice of the block that we want to read to the buffer.
+            memcpy(buf_original, buf_bloque + desp1, nbytes);
+            
+            // Return the number of bytes read.
+            return nbytes;
         } 
         else
         {
-            read_bytes += BLOCKSIZE - desp1;
-            //not finished
+            // If the block is not allocated, we dont change the
+            // buffer (because it is full of 0's) and return nbytes
+            // as the number of bytes read.
+            return nbytes;
         }
 
     }
-    
+    // Case where the reading affects to more than 1 block.
     else
     {
-        //not finished
+        // FASE 1: First logical block (the important info is BLOCSIZE - desp1)
+        int nbfisico = traducir_bloque_inodo(&inode, primerBL, 0);
+        
+        // Buffer that we will use to read the slize of the fist and last block.
+        char buf_bloque[BLOCKSIZE];
+        
+        // If the block doesn't exist, we don't read it.
+        if (nbfisico != -1)
+        {
+            if (bread(nbfisico, buf_bloque) == FALLO) return FALLO;
+            memcpy(buf_original, buf_bloque + desp1, BLOCKSIZE - desp1);   
+        }
+        
+        // Add the number of bytes read to the total number of bytes read.
+        read_bytes += BLOCKSIZE - desp1;
+
+        // FASE 2: Intermediate logical blocks (all the block is info that we want to read).
+        for (size_t i = primerBL + 1; i < ultimoBL; i++)
+        {
+            if (nbfisico = traducir_bloque_inodo(&inode, i, 0) != -1)
+            {
+                // Read the block, storing it in the buffer.
+                if (bread(nbfisico, buf_original + (BLOCKSIZE - desp1) + (i - primerBL - 1)) == FALLO) return FALLO;   
+            }
+            // Add the number of bytes read (BLOCKSIZE) to the total number of bytes read.
+            read_bytes += BLOCKSIZE;
+        }
+        
+        // FASE 3: Last logical block.
+
+        if (nbfisico = traducir_bloque_inodo(&inode, ultimoBL, 0) != -1)
+        {
+            // Read the last block where the data is stored.
+            if (bread(nbfisico, buf_bloque) == FALLO) return FALLO;
+            // Copy the slice of the block that we want to read to the buffer.
+            memcpy(buf_original + read_bytes, buf_bloque, desp2 + 1);
+        }
+        // Add the number of bytes read to the total number of bytes read.
+        read_bytes += desp2;
+        return read_bytes;
     }
     
 }
 
 /// @brief Returns the meta-information of a file/directory: type, permissions, etc.
+/// This information is placed at p_stat.
 /// @param ninodo inode number
 /// @param p_stat STAT where the meta-information is stored
 /// @return EXITO if completed succesfully, FALLO if there is an error
@@ -152,7 +212,9 @@ int mi_chmod_f(unsigned int ninodo, unsigned char permisos)
 {
     struct inodo inode;
     if(leer_inodo(ninodo, &inode) == FALLO) return FALLO;
+    inode.permisos = permisos;
     inode.ctime = time(NULL);
     if(escribir_inodo(ninodo, &inode) == FALLO) return FALLO;
+
     return EXITO;
 }
