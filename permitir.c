29de3e740847b6cbabce90200faa6b1892394f8e
jcasben <jcasben02@gmail.com>
2024-03-01 17:16:35 +0100

    [UPDATE] Finished permitir.c

--- permitir.c
+++ permitir.c
@@ -3,4 +3,41 @@
 int main(int argc, char **args)
 {
     // TODO: permitir.c implementation
+    if (argc != 4)
+    {
+        fprintf(
+            stderr,
+            RED
+            "Syntax error: ./permitir <nombre_dispositivo> <ninodo> <permisos>\n"
+            RESET
+        );
+        return FALLO;  
+    }
+
+    if (bmount(args[1]) == FALLO)
+    {
+        fprintf(
+            stderr,
+            RED
+            "Error: unable to mount the file system\n"
+            RESET
+        );
+        return FALLO;
+    }
+
+    int ninode = atoi(args[2]);
+    int permissions = atoi(args[3]);
+    mi_chmod_f(ninode, permissions);
+
+    if (bumount() == FALLO)
+    {
+        fprintf(
+            stderr,
+            RED
+            "Error: unable to unmount the file system\n"
+            RESET
+        );
+        return FALLO;
+    }
+    
 }
