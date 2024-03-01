f80570b665d1c8160ffbb91153c9de1363a8a801
linkcla <linkcla10@gmail.com>
2024-03-01 15:52:32 +0100

    [NEW] Started leer.c

--- leer.c
+++ leer.c
@@ -3,4 +3,19 @@
 int main(int argc, char **args)
 {
     // TODO: leer.c implementation
+    unsigned int ninodo;
+
+    if (argc != 2)
+    {
+        fprintf(
+            stderr, 
+            RED 
+            "Usage: ./leer <nombre_dispositivo> <ninodo>\n" 
+            RESET
+        );
+        return FALLO;
+    }
+    
+    ninodo = args[1];
+
 }
