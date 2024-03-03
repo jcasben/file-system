d17afdbafcbff97b374ec2944fef097bfcc79c1e
linkcla <linkcla10@gmail.com>
2024-03-03 17:52:55 +0100

    [NEW] Added new functions (lvl6)

--- ficheros.h
+++ ficheros.h
@@ -18,3 +18,4 @@ int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offse
 int mi_read_f(unsigned int ninodo, void *buf_original, unsigned int offset, unsigned int nbytes);
 int mi_stat_f(unsigned int ninodo, struct STAT *p_stat);
 int mi_chmod_f(unsigned int ninodo, unsigned char permisos);
+int mi_truncar_f(unsigned int ninodo, unsigned int nbytes);
