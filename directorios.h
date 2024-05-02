c62243b0037a7a6145a1459ab94e3f45d611fc94
jcasben <jcasben02@gmail.com>
2024-05-02 15:30:18 +0200

    Update: added new function declarations

--- directorios.h
+++ directorios.h
@@ -11,14 +11,27 @@
 #define TAMBUFFER (TAMFILA * 1000)
 
 #define TAMNOMBRE 60 //tamaño del nombre de directorio o fichero, en Ext2 = 256
+#define PROFUNDIDAD 32 //profundidad máxima del árbol de directorios
+
 struct entrada {
     char nombre[TAMNOMBRE];
     unsigned int ninodo;
 };
+
+struct UltimaEntrada{
+  char camino [TAMNOMBRE*PROFUNDIDAD];
+  int p_inodo;
+};
+
 
 int extraer_camino(const char *camino, char *inicial, char *final, char *tipo);
 int buscar_entrada(const char *camino_parcial, unsigned int *p_inodo_dir, unsigned int *p_inodo, unsigned int *p_entrada, char reservar, unsigned char permisos);
 void mostrar_error_buscar_entrada(int error);
 int mi_creat(const char *camino, unsigned char permisos);
 int mi_dir(const char *camino, char *buffer, char tipo, char flag);
+int mi_chmod(const char *camino, unsigned char permisos);
+int mi_stat(const char *camino, struct STAT *p_stat);
 void mostrar_buscar_entrada(char *camino, char reservar);
+int mi_write(const char *camino, const void *buf, unsigned int offset, unsigned int nbytes);
+int mi_read(const char *camino, void *buf, unsigned int offset, unsigned int nbytes);
+
