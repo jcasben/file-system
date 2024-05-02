97c108281bb9807fcd6ae527b8080578abc4d9ef
jcasben <jcasben02@gmail.com>
2024-05-02 15:11:25 +0200

    Update: finished mi_mkdir

--- mi_mkdir.c
+++ mi_mkdir.c
@@ -13,5 +13,29 @@ int main(int argc, char **args)
         return FALLO;
     }
 
-    
+    if (args[3][strlen(args[3]) - 1] != '/')
+    {
+        fprintf(
+            stderr,
+            RED
+            "Unable to create directory with specified path (path has to end with '/'). Try to use ./mi_touch instead\n"
+            RESET
+        );
+        return FALLO;
+    }
+
+    int permisos = atoi(args[2]);
+
+    if (permisos < 3 || permisos > 7)
+    {
+        fprintf(stderr, RED "ERROR: the permissions must be in between 3 and 7\n" RESET);
+    }
+
+    if (bmount(args[1]) < 0) return FALLO;
+
+    mi_creat(args[3], permisos);
+
+    if (bumount(args[1]) < 0) return FALLO;
+
+    return EXITO;
 }
