d1e6a9193273b9269a2b91926057bbb32805dbbc
jcasben <jcasben02@gmail.com>
2024-05-04 23:52:51 +0200

    Fix: corrected error in mi_creat

--- directorios.c
+++ directorios.c
@@ -241,8 +241,11 @@ int mi_creat(const char *camino, unsigned char permisos)
 {
     struct superbloque SB;
     bread(posSB, &SB);
+    unsigned int p_inode_dir = SB.posInodoRaiz;
+    unsigned int n_inode = 0;
+    unsigned int p_entry = 0;
     mostrar_error_buscar_entrada(
-        buscar_entrada(camino, &SB.posInodoRaiz, 0, 0, 1, permisos)
+        buscar_entrada(camino, &p_inode_dir, &n_inode, &p_entry, 1, permisos)
     );
 
     return EXITO;
