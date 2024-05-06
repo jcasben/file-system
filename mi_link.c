aeec86882456f4e33c5ad0acb13d502dc03f5c94
jcasben <jcasben02@gmail.com>
2024-05-06 22:36:22 +0200

    New: created mi_link

---
+++ mi_link.c
@@ -0,0 +1,62 @@
+/*
+    Autores:
+        - Marc Link Cladera
+        - Carlos Gálvez Mena
+        - Jesús Castillo Benito
+*/
+
+#include "directorios.h"
+
+int main(int argc, char **argv)
+{
+    if(argc != 4)
+    {
+        fprintf(stderr,
+                RED
+                "Syntaxis error: ./mi_link disco /ruta_fichero_original /ruta_enlace\n"
+                RESET);
+        return FALLO;
+    }
+
+    if (bmount(argv[1]) < 0)
+    {
+        fprintf(stderr,
+                RED
+                "Error mounting the device\n"
+                RESET);
+        return FALLO;
+    }
+
+    if (argv[2][strlen(argv[2]) - 1] == '/')
+    {
+        fprintf(stderr,
+                RED
+                "ERROR: the original file cannot be a directory\n"
+                RESET);
+        return FALLO;
+    }
+
+    if (argv[3][strlen(argv[3]) - 1] == '/')
+    {
+        fprintf(stderr,
+                RED
+                "ERROR: the link route cannot be a directory route\n"
+                RESET);
+        return FALLO;
+    }
+
+    if (mi_link(argv[2], argv[3]) < 0)
+    {
+        return FALLO;
+    }
+
+    if (bumount() < 0)
+    {
+        fprintf(stderr,
+                RED
+                "Error unmounting the device\n"
+                RESET);
+        return FALLO;
+    }
+    return EXITO;
+}
