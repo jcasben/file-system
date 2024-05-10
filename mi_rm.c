196114c4c15b31362046649733ef16a591573e50
jcasben <jcasben02@gmail.com>
2024-05-10 15:41:49 +0200

    Update: added error handling

--- mi_rm.c
+++ mi_rm.c
@@ -9,12 +9,29 @@
 
 int main(int argc, char **args)
 {
+    if (argc != 3)
+    {
+        fprintf(stderr, RED "ERROR: Incorrect number of arguments. Syntaxis: ./mi_rm disco /ruta \n" RESET);
+        return FALLO;
+    }
+
+    if (args[2][strlen(args[2]) - 1] == '/')
+    {
+        fprintf(stderr, RED "ERROR: With mi_rm you can only delete files, if you want to delete a direcotry use mi_rmdir\n" RESET);
+        return FALLO;
+    }
+
     if (bmount(args[1]) < 0)
     {
         fprintf(stderr, RED "Error mounting the device\n" RESET);
         return FALLO;
     }
 
+    if (mi_unlink(args[2]) < 0)
+    {
+        fprintf(stderr, RED "Error deleting the file\n" RESET);
+        return FALLO;
+    }
 
     if (bumount() < 0)
     {
