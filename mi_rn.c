b5030c2557a2798f54813a03e45d6f8a3c63fd57
jcasben <jcasben02@gmail.com>
2024-05-13 18:20:00 +0200

    Feat: created mi_rn

---
+++ mi_rn.c
@@ -0,0 +1,42 @@
+/*
+    Autores:
+        - Marc Link Cladera
+        - Carlos Gálvez Mena
+        - Jesús Castillo Benito
+*/
+
+#include "directorios.h"
+
+int main(int argc, char **args)
+{
+    if (argc != 4)
+    {
+        fprintf(
+                stderr,
+                RED
+                "ERROR: invalid syntax. Usage: ./mi_rn <disco> </ruta_antigua> <ruta_nueva>\n"
+                RESET
+        );
+        return FALLO;
+    }
+
+    if (bmount(args[1]) < 0) return FALLO;
+    unsigned int p_inodo_dir = 0;
+    unsigned int n_inode = 0;
+    unsigned int p_entry = 0;
+    int error = buscar_entrada(args[2], &p_inodo_dir, &n_inode, &p_entry, 0, 6);
+    if(error < 0)
+    {
+        mostrar_error_buscar_entrada(error);
+        return FALLO;
+    }
+
+    struct entrada entrada;
+    if(mi_read_f(p_inodo_dir, &entrada, p_entry * sizeof(struct entrada),
+            sizeof(struct entrada)) == FALLO) return FALLO;
+    strcpy(entrada.nombre, args[3]);
+    if(mi_write_f(p_inodo_dir, &entrada, p_entry * sizeof(struct entrada),
+            sizeof(struct entrada)) == FALLO) return FALLO;
+
+    if (bumount() < 0) return FALLO;
+}
