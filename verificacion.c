f3808cc8fa3bda50b72595e05258cd7692fa0353
jcasben <jcasben02@gmail.com>
2024-05-30 20:35:41 +0200

    [UPDATE]: changed time stamps to timeval

--- verificacion.c
+++ verificacion.c
@@ -48,7 +48,10 @@ int main(int argc, char **args)
     while (index < num_entradas)
     {
         struct INFORMACION info;
+        memset(&info, 0, sizeof(struct INFORMACION));
         char file_buffer[256];
+        memset(file_buffer, 0, sizeof(file_buffer));
+
         directorio = entradas[index];
         sprintf(file_buffer, "%s%s/prueba.dat", file, directorio.nombre);
         info.pid = (pid_t) atoi(strchr(directorio.nombre, '_') + 1);
@@ -96,25 +99,47 @@ int main(int argc, char **args)
 
         // Obtener la escritura de la última posición.
         // Añadir la información del struct info al fichero informe.txt por el final.
-        char buffer_info[BLOCKSIZE];
+        char buffer_info[490];
         memset(buffer_info, 0, sizeof(buffer_info));
+
+        // Buscado en ChatGPT como poner la fecha con milisegundos
+        char aux1[30];
+        char aux2[30];
+        char aux3[30];
+        char aux4[30];
+        struct tm *tm_info1;
+        struct tm *tm_info2;
+        struct tm *tm_info3;
+        struct tm *tm_info4;
+
+        tm_info1 = localtime(&info.PrimeraEscritura.fecha.tv_sec);
+        tm_info2 = localtime(&info.UltimaEscritura.fecha.tv_sec);
+        tm_info3 = localtime(&info.MenorPosicion.fecha.tv_sec);
+        tm_info4 = localtime(&info.MayorPosicion.fecha.tv_sec);
+
+        strftime(aux1, 30, "%Y-%m-%d %H:%M:%S", tm_info1);
+        strftime(aux2, 30, "%Y-%m-%d %H:%M:%S", tm_info2);
+        strftime(aux3, 30, "%Y-%m-%d %H:%M:%S", tm_info3);
+        strftime(aux4, 30, "%Y-%m-%d %H:%M:%S", tm_info4);
+
+
         sprintf(buffer_info,
             "PID: %d\n"
             "Numero de escrituras: %d\n"
-            "Primera escritura \t%d\t%d\t%s"
-            "Ultima escritura  \t%d\t%d\t%s"
-            "Menor posicion    \t%d\t%d\t%s"
-            "Mayor posicion    \t%d\t%d\t%s\n",
+            "Primera escritura \t%d\t%d\t%s.%06ld\n"
+            "Ultima escritura  \t%d\t%d\t%s.%06ld\n"
+            "Menor posicion    \t%d\t%d\t%s.%06ld\n"
+            "Mayor posicion    \t%d\t%d\t%s.%06ld\n\n",
             info.pid,
             escrituras_validadas,
-            info.PrimeraEscritura.nEscritura, info.PrimeraEscritura.nRegistro, asctime(localtime(&info.PrimeraEscritura.fecha)),
-            info.UltimaEscritura.nEscritura, info.UltimaEscritura.nRegistro, asctime(localtime(&info.UltimaEscritura.fecha)),
-            info.MenorPosicion.nEscritura, info.MenorPosicion.nRegistro, asctime(localtime(&info.MenorPosicion.fecha)),
-            info.MayorPosicion.nEscritura, info.MayorPosicion.nRegistro, asctime(localtime(&info.MayorPosicion.fecha)));
+            info.PrimeraEscritura.nEscritura, info.PrimeraEscritura.nRegistro, aux1, info.PrimeraEscritura.fecha.tv_usec,
+            info.UltimaEscritura.nEscritura, info.UltimaEscritura.nRegistro, aux2, info.UltimaEscritura.fecha.tv_usec,
+            info.MenorPosicion.nEscritura, info.MenorPosicion.nRegistro, aux3, info.MenorPosicion.fecha.tv_usec,
+            info.MayorPosicion.nEscritura, info.MayorPosicion.nRegistro, aux4, info.MayorPosicion.fecha.tv_usec);
 
         // Escribir buffer_info en el fichero informe.txt
         mi_write(ruta, buffer_info, index * sizeof(buffer_info), sizeof(buffer_info));
-        
+
         memset(buffer_escrituras, 0, sizeof(buffer_escrituras));
         #if DEBUGN13
         fprintf(
